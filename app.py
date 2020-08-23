a5633ca9a64c23c4653fd4c2379171ec936ac3a3
Lauren Stein <lmstein22@gmail.com>
2020-08-23 12:38:15 -0400

    pdf for top station graph and adding routes to the app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+#Import Flask
+from flask import Flask
+
+#Create an app
+app = Flask(__name__)
+
+#Define route
+@app.route("/")
+def home():
+    #list available routes
+
+@app.route("/api/v1.0/precipitation")
+
+@app.route("/api/v1.0/stations")
+
+@app.route("/api/v1.0/tobs")
+
+@app.route("/api/v1.0/<start>")
+
+@app.route("/api/v1.0/<start>/<end>")
