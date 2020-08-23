bbed38d95dbb8f1887d5f982fb1f4759aef11991
Lauren Stein <lmstein22@gmail.com>
2020-08-23 13:28:10 -0400

    moved some #code for dictionary in ipynb and added rain and stations api for app.py

--- app.py
+++ app.py
@@ -1,20 +1,111 @@
-#Import Flask
-from flask import Flask
+import numpy as np
 
-#Create an app
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+conn = engine.connect()
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+measurement = Base.classes.measurement
+hawaii_station = Base.classes.station
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
-#Define route
+
+#################################################
+# Flask Routes
+#################################################
+
 @app.route("/")
-def home():
-    #list available routes
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
 
 @app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    results = session.query(measurement.station, measurement.date, measurement.prcp).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    rain = []
+    for station, date, prcp in results:
+        measurement_dict = {}
+        measurement_dict["station"] = station
+        measurement_dict["date"] = date
+        measurement_dict["prcp"] = prcp
+        rain.append(measurement_dict)
+
+    return jsonify(rain)
 
 @app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-@app.route("/api/v1.0/tobs")
+    results = session.query(hawaii_station.station, hawaii_station.name).all()
 
-@app.route("/api/v1.0/<start>")
+    session.close()
 
-@app.route("/api/v1.0/<start>/<end>")
+
+    stations_list = []
+    for station, name in results:
+        stations_dict = {}
+        stations_dict["station_id"] = station
+        stations_dict["name"] = name
+        stations_list.append(stations_dict)
+
+    return jsonify(stations_list)
+
+# @app.route("/api/v1.0/passengers")
+# def passengers():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of passenger data including the name, age, and sex of each passenger"""
+#     # Query all passengers
+#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+#     session.close()
+
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_passengers = []
+#     for name, age, sex in results:
+#         passenger_dict = {}
+#         passenger_dict["name"] = name
+#         passenger_dict["age"] = age
+#         passenger_dict["sex"] = sex
+#         all_passengers.append(passenger_dict)
+
+#     return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
